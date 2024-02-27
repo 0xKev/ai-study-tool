1a84d390f0ce5cef3134e7cb4dbfb9b2e7e33a95
0xKev <56137695+0xKev@users.noreply.github.com>
2024-02-26 23:23:30 -0500

    cleared git cache - modified gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("pdf_files\The_Happy_Prince by Oscar Wilde.pdf") as pdf:
-    allPages = len(pdf.pages)
-
-    for page in range(allPages):
-        print(pdf.pages[page].extract_text(x_tolerance=3, x_tolerance_ratio=None, y_tolerance=3, layout=False, x_density=7.25, y_density=13))
