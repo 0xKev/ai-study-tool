0a1b97a3b85afe5aa39d375b86cdc3f014635844
0xKev <56137695+0xKev@users.noreply.github.com>
2024-03-01 03:00:59 -0500

    untracked pdf files for git

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 import streamlit as st
-from pdf_utils import PdfProcessor
+from pdf_utils import PdfProcessor, get_text_chunks
 import tempfile # to store pdfs in temp folder so I can have a file path for now to feed to PdfProcessor
 import os
+import time
 
+def progress_bar():
+    progress_text = "Your PDF is being processed. Please wait..."
+    my_bar = st.progress(0, progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.001)
+        my_bar.progress(percent_complete + 1, progress_text)
+    time.sleep(1)
+    my_bar.empty()
+        
 def main():
     st.set_page_config(page_title="Noteify")
 
@@ -15,19 +25,33 @@ def main():
 
     with st.sidebar:
         st.header("Document Upload")
-        pdf_file = st.file_uploader("Choose your PDFs and click 'Process'", type=["pdf"], accept_multiple_files=True)
+        uploaded_pdf_file = st.file_uploader("Choose your PDFs and click 'Process'", type=["pdf"], accept_multiple_files=False) # false for now, if true will need to loop upload_pdf_file
 
-        if st.button("Process"):
-            if pdf_file:
+        if st.button("Process", key="process_pdf"):
+            if uploaded_pdf_file:
                 with st.spinner("Processing..."):
-                    # create temp folder to store pdfs
-                    temp_folder = tempfile.TemporaryDirectory()
-                    file_path = os.path.join(temp_folder.name, pdf_file.name)
-                    with open(file_path, "wb") as f:
-                        f.write(pdf_file.getvalue())
+                    # i want to store the pdf in a temp folder so i can get the file path and feed it to PdfProcessor
+                    temp_dir = tempfile.TemporaryDirectory()
+                    temp_file_path = os.path.join(temp_dir.name, uploaded_pdf_file.name)
 
-                    pdf = PdfProcessor(file_path)
+                    with open(temp_file_path, "wb") as f:
+                        f.write(uploaded_pdf_file.getbuffer())
+
+                    
+                    raw_text = PdfProcessor(temp_file_path).getAll()
+                    
+
+                    text_chunks = get_text_chunks(raw_text)
+                    st.write(text_chunks)
+                    '''for i in range(len(text_chunks)):
+                        st.write(f"Chunk {i + 1}: {text_chunks[i]}")'''
+                    
                     st.success("Processing complete.")
+    
+    if st.button("Start progress bar", key="start_progress_bar"):
+        progress_bar()
+
+    
 
 
 if __name__ == "__main__":
--- app/pdf_utils.py
+++ app/pdf_utils.py
@@ -1,6 +1,7 @@
 # will unpack the pdf to text
 import pdfplumber
-
+import time
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 '''
 user uploads pdf
@@ -10,7 +11,7 @@ pdf class features:
 - fetches txt for any page user requests
 
 '''
-
+h
 
 class PdfProcessor:
     #constructor
@@ -19,9 +20,17 @@ class PdfProcessor:
         self.uploaded_by = uploaded_by
         self.extracted_text = ''
 
-        with pdfplumber.open(self.file_path) as pdf:
-            self.totalPages = len(pdf.pages)
-          
+        try:
+
+            with pdfplumber.open(self.file_path) as pdf:
+                self.totalPages = len(pdf.pages)
+
+        except FileNotFoundError as invalid_file_path:
+
+            return (f"File not found: {invalid_file_path}")
+        
+        except TypeError as type_error:
+            return (f"Invalid file type: {type_error}")
 
     def NumPages(self) -> int:
         return self.totalPages
@@ -34,29 +43,27 @@ class PdfProcessor:
         extracted_text = ''
         
         with pdfplumber.open(self.file_path) as pdf:
-            if page_num is None:
-                if end_page is None:
-                    page_num = 0                    # for 0 based indexing
-                    end_page = self.totalPages      # to include last page + 1
-            else:                                   # if page_num then set end_page 
-                end_page = page_num + 1             # to include last page
+            if page_num is None and end_page is None:
+                page_num = 1                    # for 0 based indexing
+                end_page = self.totalPages      # self.totalPages is 1 based indexing so no need to add 1
+            elif end_page is None:                                   # if page_num then set end_page 
+                end_page = page_num             # to compensate for 0 based indexing below
 
             # input validation if pages are specified
-            if page_num < 0 or page_num > self.totalPages:
-                raise ValueError("Invalid page number.")
+            if page_num <= 0 or page_num > self.totalPages:
+                raise ValueError(f"Invalid page number. Make sure it's within the range 1 to {self.totalPages}.")
             
             elif end_page > self.totalPages or end_page < page_num:
-                raise ValueError("Invalid end page.")
+                raise ValueError(f"Invalid end page. Did you mean page {self.totalPages}?")
             
             '''print(f"start page: {page_num}")
             print(f"end page {end_page}")'''
 
-            for page in range(page_num, end_page):
+            for page in range(page_num - 1, end_page):  # for 0 based indexing
                 extracted_text += pdf.pages[page].extract_text(x_tolerance=3, x_tolerance_ratio=None, y_tolerance=3, layout=False, x_density=7.25, y_density=13)
                 #extracted_text += f"\n{'--' * 30}\n"
         
             return extracted_text
-    
     
     def getPagesInChunks(): # if big pdf, then process every 5 pages, serve info, and repeat
         pass
@@ -68,16 +75,36 @@ class PdfProcessor:
         extracted_text = self.getPages()
         
         return extracted_text
+    
+
 class NoteSummarizer:
     pass
 
-'''reader = PdfReader("pdf_files\The_Happy_Prince by Oscar Wilde.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()'''
+def get_text_chunks(raw_text: str) -> list:
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
 
 if __name__ == "__main__":
+    start_time = time.time()
 
-    the_happy_prince = PdfProcessor("..\pdf_files\The_Happy_Prince by Oscar Wilde.pdf")
-    #print(the_happy_prince.getPages(4, 5))
+    pdf_file_location = "..\pdf_files\The_Happy_Prince by Oscar Wilde.pdf"
+    pdf_file = PdfProcessor(pdf_file_location)
+
+    raw_text = pdf_file.getPages(1, 5)
+    text_chunks = get_text_chunks(raw_text)
+
+    #print(raw_text.getPages(1, 5))
+    
+    for i in range(len(text_chunks)):
+        print(f"Chunk {i + 1}: {text_chunks[i]}\n")
+
+    print("end time:", time.time() - start_time)
+    
     #print(the_happy_prince.getAll)
